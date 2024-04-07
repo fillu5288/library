58554344f2101ad73592c108a00eb913696863b9
Andrey <kamisatofillu@gmail.com>
2024-04-07 09:11:24 +0400

    Repaired version

---
+++ src/main/java/org/mksmv/Library/dao/PersonDAO.java
@@ -0,0 +1,50 @@
+package org.mksmv.Library.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.mksmv.Library.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM person",
+                new BeanPropertyRowMapper<>(Person.class));
+    } // получение данных
+
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM person WHERE email=?", new Object[] {email},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM person WHERE id=?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO person(name, age, email) VALUES(?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE person SET name=?, age=?, email=? WHERE id=?",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getAddress(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
+    }
+}
